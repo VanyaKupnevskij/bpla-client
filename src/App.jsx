2a4d6f19489d54a57be51d010e51eed806111410
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-05-11 21:57:32 +0300

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import './App.css';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useRoutes } from './routes';
+import { useAuth } from './hooks/auth.hook';
+import { AuthContext } from './context/context';
+import { ConfigContext } from './context/configContext';
+import Loader from './components/Loader';
+
+import { ThemeProvider } from '@mui/material/styles';
+import useTheme from './hooks/theme.hook';
+
 function App() {
-  return <div>HELLO</div>;
+  const { token, login, logout, userId, ready, isAuthenticated } = useAuth(); // bplaPassAdmin - password; admin123@gmail.com email
+  const routes = useRoutes(isAuthenticated);
+  const { themeDark, themeLight, modeView, setModeView } = useTheme();
+
+  if (ready === false) {
+    return <Loader />;
+  }
+
+  return (
+    <ConfigContext.Provider value={{ modeView, setModeView }}>
+      <AuthContext.Provider
+        value={{
+          token,
+          login,
+          logout,
+          userId,
+          isAuthenticated,
+        }}>
+        <ThemeProvider theme={modeView === 'light' ? themeLight : themeDark}>
+          <Router>{routes}</Router>
+        </ThemeProvider>
+      </AuthContext.Provider>
+    </ConfigContext.Provider>
+  );
 }
 
 export default App;
